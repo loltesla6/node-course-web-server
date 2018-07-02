453f0af64a385af6fe00236bb8e7f9d36652b45b
Saurabh <dev.006loltesla@gmail.com>
2018-07-02 20:34:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('view engine', 'hbs'); //tells express which view engine to use
+
+
+app.use((req,res,next)=>{          //next is used ti tell when middlewear is over
+
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url} `;
+
+fs.appendFile('server.log',log + '\n',(err)={
+  if(err){console.log('unable to append server.log');}
+});
+next();
+})
+
+// app.use((req,res,next)=>{
+//
+//   res.render('maintenance.hbs');
+// })
+app.use(express.static(__dirname + '/public'));//isko maintenance vaale part ke neeche he use karna chaheye
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear
+});
+
+app.get('/', (req,res)=>{
+  // res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    message: 'Welcome User',
+  //  currentYear: new Date().getFullYear() we are using a helper instead of this
+  });
+});
+
+app.get('/about', (req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About page',
+  //  currentYear : new Date().getFullYear()  we are using a helper instead of this
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    error: 'unable to follow the request'
+  })
+})
+app.listen(3000 , ()=>{
+  console.log('server is up on port 3000');
+});
